8fb898749fe4b6083319e40c50d2e27b5a1ddbff
Maximiliano Sanchez <makzofx@gmail.com>
2022-11-18 18:52:02 -0300

    actualizando la validacion recap

--- StyleBus/recap.js
+++ StyleBus/recap.js
@@ -20,7 +20,7 @@ let listUsuariosRECAP = [
 
 
 const expresiones = {
-    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+    usuario: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, // EMAIL VALIDO
     nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
     password: /^.{4,12}$/, // 4 a 12 digitos.
     correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
@@ -41,7 +41,11 @@ function confirmarPago() {
 const validarFormulario = (e) => {
     switch (e.target.name) {
         case "nombre":
-            console.log("Funciona nombre");
+            if (expresiones.usuario.test(e.target.value)){
+
+            }else{
+                
+            }
             break
         case "apellido":
             console.log("Funciona apellido");
